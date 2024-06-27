77329073d1e5651bf8005880acbf33c089768c4e
bambooOctopus <bamboooctopus@gmail.com>
2024-06-27 15:39:23 -0400

    currentDate state in contentContainer; date picker complete

--- src/components/dash/ContentContainerComponent.jsx
+++ src/components/dash/ContentContainerComponent.jsx
@@ -1,10 +1,23 @@
+import { useState } from "react"
+import { useEffect } from "react"
+import { DateTime } from "luxon"
+
 import { DateNav } from "./DateNavComponent"
 import { HabitBar } from "./HabitBarComponent"
 
 export const ContentContainer = () => {
+    const [ currentDate, setCurrentDate ] = useState("")
+
+    useEffect(() => {
+        const dt = DateTime.now()
+        const dateString = dt.toLocaleString({month: 'long', day: 'numeric'})
+        {currentDate == "" ? setCurrentDate(dateString) : null}        
+        
+    }, [])
+
     return (
         <div className="content-container">
-            <DateNav />
+            <DateNav currentDate={currentDate} setCurrentDate={setCurrentDate} />
             <HabitBar />
             <div className="content"></div>
         </div>
--- src/components/dash/DateNavComponent.jsx
+++ src/components/dash/DateNavComponent.jsx
@@ -1,7 +1,41 @@
-export const DateNav = () => {
+
+import { DateTime } from "luxon"
+
+export const DateNav = ({currentDate, setCurrentDate}) => {
+    const leftButton = "<"
+    const rightButton = ">"
+
+    const handleYesterdayClick = (event) => {
+        event.preventDefault()        
+        // format currentDate into date obj
+        // minus one day
+        // set new currentDate
+        const formattedDate = DateTime.fromFormat(currentDate, "MMMM dd")
+        const newDate = formattedDate.minus({days: 1})
+        const newDateString = newDate.toLocaleString({month: 'long', day: 'numeric'})
+        setCurrentDate(newDateString)
+    }
+
+    const handleTomorrowClick = (event) => {
+        event.preventDefault()
+        // format currentDate into date obj
+        // add one day
+        // set new currentDate
+        const formattedDate = DateTime.fromFormat(currentDate, "MMMM dd")
+        const newDate = formattedDate.plus({days: 1})
+        const newDateString = newDate.toLocaleString({month: 'long', day: 'numeric'})
+        setCurrentDate(newDateString)
+        
+    }
+    
+    
+
+
     return (
         <div className="date-nav">
-            <p>date mover</p>
+            <button onClick={handleYesterdayClick} className="date-button">{leftButton}</button>
+            <p>{currentDate}</p>
+            <button onClick={handleTomorrowClick} className="date-button">{rightButton}</button>
         </div>
     )
 }
